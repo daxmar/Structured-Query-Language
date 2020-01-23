2f21c450f6709357e87a5c0f2a53370873865445
Brq Dagmar Frahadixta <brqdagmar@gmail.com>
2020-01-23 13:11:25 +0700

    Update showemployee.php

--- showemployee.php
+++ showemployee.php
@@ -9,20 +9,20 @@
 	//Create SQL Query where the employee are spesifik by an ID
 	$sql = "SELECT * FROM employee WHERE id=$id";
 	
-	//Mendapatkan Hasil 
+	//Get result
 	$r = mysqli_query($con,$sql);
 	
-	//Memasukkan Hasil Kedalam Array
+	//Call result to the array
 	$result = array();
 	$row = mysqli_fetch_array($r);
 	array_push($result,array(
 			"id"=>$row['id'],
 			"name"=>$row['nama'],
-			"desg"=>$row['posisi'],
-			"salary"=>$row['gajih']
+			"pos"=>$row['position'],
+			"salary"=>$row['salary']
 		));
  
-	//Menampilkan dalam format JSON
+	//Show on JSON Format
 	echo json_encode(array('result'=>$result));
 	
 	mysqli_close($con);
