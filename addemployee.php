5dbe979ed16fbbc6b60408b9fe51aa0f801a0e3c
Brq Dagmar Frahadixta <brqdagmar@gmail.com>
2020-01-23 11:19:36 +0700

    Create addemployee.php

---
+++ addemployee.php
@@ -0,0 +1,25 @@
+<?php
+
+	if($_SERVER['REQUEST_METHOD']=='POST'){
+		
+		//Get Variable value
+		$name = $_POST['name'];
+		$pos = $_POST['position'];
+		$sal = $_POST['salary'];
+		
+		//SQL Syntax
+		$sql = "INSERT INTO addemployee (name,position,salary) VALUES ('$name','$pos','$sal')";
+		
+		//Import File Conection to the database
+		require_once('Conection.php');
+		
+		//Execution Query database
+		if(mysqli_query($con,$sql)){
+			echo 'Success added employee';
+		}else{
+			echo 'Failed to add employee';
+		}
+		
+		mysqli_close($con);
+	}
+?>
